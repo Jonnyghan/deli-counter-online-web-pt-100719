4032be289ca2727d983ef763868ad01f66abea5f
Jonathan Ghandforoush <jonnyghan@gmail.com>
2019-10-18 20:01:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,35 @@
+require 'pry'
 # Write your code here.
+katz_deli = []
+def line(array)
+  namestring = ""
+  if array.length == 0 
+    puts "The line is currently empty."
+  else 
+    namearray= []
+    counter = 1
+    array.each do |name| namearray.push("#{counter}. #{name}")
+    counter += 1 
+    namestring = namearray.join(" ") 
+
+  
+  end
+    puts "The line is currently: #{namestring}"
+  end
+end
+
+def take_a_number(array,string)
+  if array.length == 0
+    array.push(string)
+  counter = 1
+  puts "Welcome, #{string}. You are number #{counter} in line."
+else
+  counter = 1
+  array.push(string)
+  array.each do |name| puts "Welcome, #{name}. You are number #{counter} in line."
+  counter += 1
+  binding.pry
+      
+    end
+  end
+end
